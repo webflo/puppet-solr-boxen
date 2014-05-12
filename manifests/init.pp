6245689d44f6ba04862ee8640c0be7c3eb5ae18b
Florian Weber <florian.weber@ueberbit.de>
2014-05-12 11:21:45 +0200

    Add solr 3.6

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,7 @@
 #
 #     include solr
 class solr {
+  include solr::common
   include homebrew
   include java
 
@@ -16,6 +17,7 @@ class solr {
 }
 
 class solr::version14 {
+  include solr::common
   include homebrew
   include java
 
@@ -26,5 +28,47 @@ class solr::version14 {
   package { 'boxen/brews/solr14':
     ensure  => '1.4.1-boxen1',
     require => Class['java']
+  }
+}
+
+class solr::version36 {
+  include solr::common
+  include homebrew
+  include java
+
+  $version = "3.6"
+
+  homebrew::formula { 'solr36':
+    before => Package['boxen/brews/solr36']
+  }
+
+  package { 'boxen/brews/solr36':
+    ensure  => '3.6.2-boxen1',
+    require => Class['java']
+  }
+
+  file { "/Library/LaunchDaemons/dev.solr.${version}.plist":
+    content => template("solr/dev.solr.${version}.plist.erb"),
+    group   => 'wheel',
+    owner   => 'root',
+  }
+
+  file { "/opt/boxen/data/solr/3.6":
+    ensure => 'directory'
+  }
+
+  service { "dev.solr.${version}":
+    ensure    => running,
+    subscribe => File["/Library/LaunchDaemons/dev.solr.${version}.plist"],
+    require   => Package["boxen/brews/solr36"],
+  }
+}
+
+class solr::common {
+  file { [
+    "/opt/boxen/log/solr",
+    "/opt/boxen/data/solr",
+    ]:
+    ensure => 'directory'
   }
 }
